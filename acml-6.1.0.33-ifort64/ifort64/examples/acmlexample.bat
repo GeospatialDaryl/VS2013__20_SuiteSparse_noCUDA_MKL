c722cb1fae678780596d99179e8f6e65849e5dde
Daryl Van Dyke <daryl_van_dyke@fws.gov>
2015-11-29 13:51:19 -0800

    added SS

---
+++ acml-6.1.0.33-ifort64/ifort64/examples/acmlexample.bat
@@ -0,0 +1,106 @@
+@ECHO off
+REM This batch script will compile and run an example program
+REM in the ACML examples directory. Supply the name of the
+REM example program WITHOUT the .f or .c filename extension, e.g.
+REM     acmlexample.bat cfft1d_example
+REM or
+REM     acmlexample.bat cfft1d_c_example
+
+REM Cause all variables assigned in this script to be local
+setlocal
+
+REM Location of ACML installation - modify if necessary
+set ACMLDIR=..
+
+REM Set PATH to locate ACML dll at run time
+PATH=%ACMLDIR%\lib;%PATH%
+
+REM Compilers and compile flags - modify if necessary
+set F77=ifort 
+set FLINK=ifort 
+set FFLAGS= -threads -MD 
+set FLINKFLAGS= -threads -MD
+set FLINKLIBS=
+REM We compile and link with Microsoft C. We must link with the
+REM PGI Fortran and C run time libraries also, using CLINKLIBS.
+set CC=cl
+set CFLAGS=-EHsc -MD "-I%ACMLDIR%/include"
+set CLINK=cl
+set CLINKFLAGS=-MD 
+set CLINKLIBS=
+
+REM Set these instead of the above if you want to use the PGI
+REM C compiler instead of Microsoft C. We must link with the
+REM PGI Fortran run time library also, using CLINKLIBS.
+REM set CC=pgcc
+REM set CLINK=pgcc
+REM set CLINKLIBS=
+
+REM The ACML library - modify if necessary
+set LIBACML="%ACMLDIR%\lib\libacml_dll.lib"
+
+if exist "%1%.f" (
+  set SOURCEFILE=%1%.f
+  set OBJECTFILE=%1%.obj
+  set EXEFILE=%1%.exe
+  set RESFILE=%1%.res
+  goto HANDLEFORTRAN
+)
+if exist "%1%.c" (
+  set SOURCEFILE=%1%.c
+  set OBJECTFILE=%1%.obj
+  set EXEFILE=%1%.exe
+  set RESFILE=%1%.res
+  goto HANDLEC
+)
+if exist "%1%.cpp" (
+  set SOURCEFILE=%1%.cpp
+  set OBJECTFILE=%1%.obj
+  set EXEFILE=%1%.exe
+  set RESFILE=%1%.res
+  goto HANDLEC
+)
+goto USAGE
+
+REM Compile and run a Fortran program
+:HANDLEFORTRAN
+@ECHO on
+  %F77% %FFLAGS% -c %SOURCEFILE% /obj:%OBJECTFILE%
+  %FLINK% %FLINKFLAGS% %OBJECTFILE% %LIBACML% %FLINKLIBS% /exe:%EXEFILE%
+  %EXEFILE% > %RESFILE%
+@ECHO off
+  type %RESFILE%
+  if exist %OBJECTFILE% del %OBJECTFILE%
+goto DONE
+
+REM Compile and run a C or C++ program
+:HANDLEC
+@ECHO on
+  %CC% %CFLAGS% -c %SOURCEFILE% /Fo%OBJECTFILE%
+  %CLINK% %CLINKFLAGS% %OBJECTFILE% %LIBACML% %CLINKLIBS% /Fe%EXEFILE%
+  %EXEFILE% > %RESFILE%
+@ECHO off
+  type %RESFILE%
+  if exist %OBJECTFILE% del %OBJECTFILE%
+goto DONE
+
+:USAGE
+
+echo.
+echo                              %0%
+echo.
+echo  This batch file will compile, link and run an ACML example program.
+echo.
+echo    Usage: %0% program
+echo.
+echo  where program is the name of the example program WITHOUT the
+echo  .f, .c or .cpp extension.
+echo.
+echo  For example:    %0% cfft1d_example
+echo            or    %0% cfft1d_c_example
+echo            or    %0% sgetrf_cpp_example
+echo.
+
+goto DONE
+
+:DONE
---
+++ acml-6.1.0.33-ifort64/ifort64/examples/performance/time_sgemm.f90
@@ -0,0 +1,169 @@
+
+!   ACML version 6.0 Copyright AMD 2014
+
+    PROGRAM MAIN
+!      .. Implicit None Statement ..
+       IMPLICIT NONE
+!      .. Parameters ..
+       INTEGER, PARAMETER              :: SP = SELECTED_REAL_KIND(6,37)
+       INTEGER, PARAMETER              :: WP=KIND(0.0D0)
+       REAL (KIND=SP), PARAMETER       :: ALIM = -1.0E0_SP
+       REAL (KIND=SP), PARAMETER       :: BLIM = 1.0E0_SP
+       INTEGER, PARAMETER              :: NN = 11
+!      .. Local Scalars ..
+       REAL (KIND=SP)                  :: ALPHA, BETA
+       INTEGER                         :: J, K, M, N
+       CHARACTER (1)                   :: TRANSA, TRANSB
+!      .. Local Arrays ..
+       INTEGER                         :: ENS(NN)
+!      .. External Subroutines ..
+       EXTERNAL                           DOTIME
+!      .. Data Statements ..
+       DATA ENS/100, 300, 600, 900, 1200, 1500, 1800, 2100, 2400, 2700, 3000/
+!      .. Executable Statements ..
+       CONTINUE
+
+       DO J = 1, NN
+
+          M = ENS(J)
+
+!         To time different data, change ENS and NN above,
+!         accordingly.
+          N = M
+          K = M
+
+          TRANSA = 'N'
+          TRANSB = 'N'
+          ALPHA = 0.7E0_SP
+          BETA = 1.3E0_SP
+          CALL DOTIME(TRANSA,TRANSB,M,N,K,ALIM,BLIM,ALPHA,BETA)
+
+       END DO
+
+    END PROGRAM MAIN
+
+!-----------------------------------------------------------------------
+!-----------------------------------------------------------------------
+
+    SUBROUTINE DOTIME(TRANSA,TRANSB,M,N,K,ALIM,BLIM,ALPHA,BETA)
+!      .. Implicit None Statement ..
+       IMPLICIT NONE
+!      .. Parameters ..
+       INTEGER, PARAMETER              :: SP = SELECTED_REAL_KIND(6,37)
+       INTEGER, PARAMETER              :: WP=KIND(0.0D0)
+!      .. Scalar Arguments ..
+       REAL (KIND=SP)                  :: ALIM, ALPHA, BETA, BLIM
+       INTEGER                         :: K, M, N
+       CHARACTER (1)                   :: TRANSA, TRANSB
+!      .. Local Scalars ..
+       REAL (KIND=WP)                  :: DNFLOP, MFX, T0X, T1X, TX
+       INTEGER                         :: IFAIL1, IFAIL2, IFAIL3, LDA, LDB, LDC
+!      .. Local Arrays ..
+       REAL (KIND=SP), ALLOCATABLE     :: A(:,:), B(:,:), C(:,:)
+!      .. External Functions ..
+       REAL (KIND=WP), EXTERNAL        :: DSECND
+!      .. External Subroutines ..
+       EXTERNAL                           SGEMM, RANDMAT
+!      .. Intrinsic Functions ..
+       INTRINSIC                          REAL
+!      .. Executable Statements ..
+       CONTINUE
+
+!      Allocate space
+       LDA = M
+       LDB = K
+       LDC = M
+       ALLOCATE (A(LDA,K),STAT=IFAIL1)
+       ALLOCATE (B(LDB,N),STAT=IFAIL2)
+       ALLOCATE (C(LDC,N),STAT=IFAIL3)
+
+       IF (IFAIL1==0 .AND. IFAIL2==0 .AND. IFAIL3==0) THEN
+
+!         Number of MFLOPs in a SGEMM of size M-by-K-by-N:
+          DNFLOP = 2.0E-6_WP*REAL(M,KIND=WP)*REAL(N,KIND=WP)*REAL(K,KIND=WP)
+
+          IF (TRANSA=='N' .OR. TRANSA=='n') THEN
+             CALL RANDMAT(ALIM,BLIM,M,K,A,LDA)
+          ELSE
+             CALL RANDMAT(ALIM,BLIM,K,M,A,LDA)
+          END IF
+
+          IF (TRANSB=='N' .OR. TRANSB=='n') THEN
+             CALL RANDMAT(ALIM,BLIM,K,N,B,LDB)
+          ELSE
+             CALL RANDMAT(ALIM,BLIM,N,K,B,LDB)
+          END IF
+
+          CALL RANDMAT(ALIM,BLIM,M,N,C,LDC)
+
+!         Using SGEMM
+          T0X = DSECND()
+          CALL SGEMM(TRANSA,TRANSB,M,N,K,ALPHA,A,LDA,B,LDB,BETA,C,LDC)
+          T1X = DSECND()
+          TX = T1X - T0X
+
+!         Print the results
+          IF (TX<=0.0E0_WP) THEN
+             MFX = 0.0E0_WP
+          ELSE
+             MFX = DNFLOP/TX
+          END IF
+          WRITE (*,FMT=*) N, MFX
+       ELSE
+          WRITE (*,*) 'Array allocation failed: attempted to allocate'
+          WRITE (*,*) '   A(', LDA, ',', K, ') and'
+          WRITE (*,*) '   B(', LDB, ',', N, ') and'
+          WRITE (*,*) '   C(', LDC, ',', N, ')'
+       END IF
+
+       IF (IFAIL1==0) THEN
+          DEALLOCATE (A)
+       END IF
+       IF (IFAIL2==0) THEN
+          DEALLOCATE (B)
+       END IF
+       IF (IFAIL3==0) THEN
+          DEALLOCATE (C)
+       END IF
+
+       RETURN
+    END SUBROUTINE DOTIME
+
+    SUBROUTINE RANDMAT(ALIM,BLIM,M,N,A,LDA)
+!      .. Implicit None Statement ..
+       IMPLICIT NONE
+!      .. Parameters ..
+       INTEGER, PARAMETER              :: SP = SELECTED_REAL_KIND(6,37)
+       INTEGER, PARAMETER              :: MSEED = 624, MSTATE = 633
+!      .. Scalar Arguments ..
+       REAL (KIND=SP)                  :: ALIM, BLIM
+       INTEGER                         :: LDA, M, N
+!      .. Array Arguments ..
+       REAL (KIND=SP)                  :: A(LDA,N)
+!      .. Local Scalars ..
+       INTEGER                         :: GENID, I, INFO, LSEED, LSTATE, SUBID
+!      .. Local Arrays ..
+       INTEGER                         :: SEED(MSEED), STATE(MSTATE)
+!      .. External Subroutines ..
+       EXTERNAL                           SRANDINITIALIZE, SRANDUNIFORM
+!      .. Executable Statements ..
+       CONTINUE
+
+!      Use the Mersenne twister generator as the base generator
+       GENID = 3
+       SUBID = 1
+
+       LSTATE = MSTATE
+       LSEED = 1
+       SEED(1) = 1071958
+
+!      Initialize the base generator
+       CALL SRANDINITIALIZE(GENID,SUBID,SEED,LSEED,STATE,LSTATE,INFO)
+
+       DO I = 1, N
+!         Generate a matrix from a uniform U(ALIM,BLIM) distribution
+          CALL SRANDUNIFORM(M,ALIM,BLIM,STATE,A(1,I),INFO)
+       END DO
+
+       RETURN
+    END SUBROUTINE RANDMAT
